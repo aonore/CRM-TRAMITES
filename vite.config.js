32e48f4ad18e9ac662676a62c3212b55d38ef8f8
aonore <a_onore@hotmail.com>
2025-10-07 02:31:40 -0300

    Vite modificado

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,15 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          vendor: ['lodash', 'date-fns', 'framer-motion', 'lucide-react', 'recharts'],
+        },
+      },
+    },
+  },
 });
